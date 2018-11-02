5ff6f55443823b4ed30d3b122ddc30082ae6d23e
anhkhoa45 <khoa.1997a@gmail.com>
2018-11-02 22:18:01 +0700

    product, category table migration

--- database/migrations/2018_11_02_141859_create_table_products.php
+++ database/migrations/2018_11_02_141859_create_table_products.php
@@ -4,7 +4,7 @@ use Illuminate\Support\Facades\Schema;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Database\Migrations\Migration;
 
-class CreateUsersTable extends Migration
+class CreateTableProducts extends Migration
 {
     /**
      * Run the migrations.
@@ -13,14 +13,12 @@ class CreateUsersTable extends Migration
      */
     public function up()
     {
-        Schema::create('users', function (Blueprint $table) {
-            $table->increments('id');
-            $table->string('email')->unique();
-            $table->string('password');
+        Schema::create('products', function (Blueprint $table) {
+            $table->increments('id')->index();
             $table->string('name');
-             $table->string('avatar');
-            $table->timestamp('email_verified_at')->nullable();
-            $table->rememberToken();
+            $table->string('description')->nullable();
+            $table->string('image');
+            $table->integer('price');
             $table->timestamps();
         });
     }
@@ -32,6 +30,6 @@ class CreateUsersTable extends Migration
      */
     public function down()
     {
-        Schema::dropIfExists('users');
+        Schema::dropIfExists('products');
     }
 }
---
+++ database/migrations/2018_11_02_144436_create_table_product_category.php
@@ -0,0 +1,31 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateTableProductCategory extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('product_category', function (Blueprint $table) {
+            $table->string('product_id');
+            $table->string('category_id');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('product_category');
+    }
+}
