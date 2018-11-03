0bb6fe9ea34b9e13b0cc7bbba47ed5f154637398
Phan Trang <phantrang3006@gmail.com>
2018-11-03 12:32:08 +0700

    Fix update name & password bug

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -23,23 +23,26 @@ class UserController extends Controller
     }
 
     public function update(Request $request) {
+        \App\User::where('id', Auth::id())->update(['name' => $request->profile_username]);
         if ($request->profile_password != null) {
             if ($request->profile_password === $request->profile_password2) {
-                \App\User::where('id', Auth::id())->update(['password' => Hash::make($request->profile_password)]);
+                \App\User::where('id', Auth::id())
+                    ->update(['password' => Hash::make($request->profile_password)]);
             }
         }
-
-        $request->validate([
-            'avatar' => 'required|image|mimes:jpeg,png,jpg,gif,svg|max:2048',
-        ]);
-        $user = Auth::user();
-        $avatarName = $user->id.'_avatar.'.request()->avatar->getClientOriginalExtension();
-        $request->avatar->storeAs('avatars', $avatarName);
-        $user->avatar = $avatarName;
-        $user->save();
-        return back()
-            ->with('success','You have successfully upload image.');
-        //return view('user.profile');
+        if ($request->avatar != null) {
+            $request->validate([
+                'avatar' => 'required|image|mimes:jpeg,png,jpg,gif,svg|max:2048',
+            ]);
+            $user = Auth::user();
+            $avatarName = $user->id.'_avatar.'.request()->avatar->getClientOriginalExtension();
+            $request->avatar->storeAs('avatars', $avatarName);
+            $user->avatar = $avatarName;
+            $user->save();
+            return back()
+                ->with('success','You have successfully upload image.');
+        }
+        return back();
     }
 
     public function update_avatar(Request $request) {
