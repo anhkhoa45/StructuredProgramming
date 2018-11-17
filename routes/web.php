a59bc8913f90c2a42aad646b96f1cdc4d618b2d8
anhkhoa45 <khoa.1997a@gmail.com>
2018-11-17 15:40:11 +0700

    Update codes

--- routes/web.php
+++ routes/web.php
@@ -11,16 +11,14 @@
 |
 */
 
-Route::view('/', 'customer.home');
-
 Auth::routes();
 Route::redirect('/logout', '/');
 
-Route::get('/customer', 'Customer\IndexController@index')->name('index');
-Route::get('/customer/home', 'Customer\HomeController@index')->name('home');
-Route::get('/customer/product/detail/{prod_id}', 'Customer\ProductController@detail')->name('product_detail');
-Route::get('/customer/user/profile', 'Customer\UserController@profile');
-Route::post('/customer/user/update', 'Customer\UserController@update');
+Route::get('/', 'Customer\IndexController@index')->name('index');
+Route::get('/home', 'Customer\HomeController@index')->name('home');
+Route::get('/product/detail/{prod_id}', 'Customer\ProductController@detail')->name('product_detail');
+Route::get('/user/profile', 'Customer\UserController@profile');
+Route::post('/user/update', 'Customer\UserController@update');
 
 //Admin
 Route::get('/admin', 'Admin\IndexController@index')->name('admin');
